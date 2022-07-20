46f66862048bcc4ed6bd2502a63be8391e18fe68
cxt90730 <cxt90730@126.com>
2022-07-20 12:37:52 +0800

    add ignore

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,5 +1,31 @@
 export namespace app {
 	
+	export class ObjectHandlerResult {
+	    err: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new ObjectHandlerResult(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.err = source["err"];
+	    }
+	}
+	export class ListBucketResult {
+	    buckets: string[];
+	    err: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new ListBucketResult(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.buckets = source["buckets"];
+	        this.err = source["err"];
+	    }
+	}
 	export class Object {
 	    key: string;
 	    size: number;
@@ -143,32 +169,6 @@ export namespace app {
 		    }
 		    return a;
 		}
-	}
-	export class ObjectHandlerResult {
-	    err: string;
-	
-	    static createFrom(source: any = {}) {
-	        return new ObjectHandlerResult(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.err = source["err"];
-	    }
-	}
-	export class ListBucketResult {
-	    buckets: string[];
-	    err: string;
-	
-	    static createFrom(source: any = {}) {
-	        return new ListBucketResult(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.buckets = source["buckets"];
-	        this.err = source["err"];
-	    }
 	}
 
 }
